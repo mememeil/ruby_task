69989969935100a05ddb83c3ed7a1d49a2ab6af8
mememeil <kodhikodhi@gmail.com>
2020-01-18 17:18:50 +0900

    Write answer for Q5 to task.rb

--- task.rb
+++ task.rb
@@ -41,10 +41,13 @@ def q4
 end
 
 def q5
+  # Q5. 配列が空であれば `true`，1つ以上の要素があれば `false` を出力するコードを書いて下さい。
   array1 = []
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
+  puts !(array1.any?)
+  puts !(array2.any?)
 
 end
 
