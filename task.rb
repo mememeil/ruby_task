a32d211b8b4fa73420087a1597532a903728e5b5
mememeil <kodhikodhi@gmail.com>
2020-01-19 11:41:57 +0900

    Write answer for Q13 to task.rb

--- task.rb
+++ task.rb
@@ -155,10 +155,12 @@ def q12
 end
 
 def q13
+  # Q13. 次の `user_data` に，`update_data` の内容を反映させ，`user_data` の内容を書き換え，出力して下さい。
   user_data = { name: "神里", age: 31, address: "埼玉" }
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
+  p user_data.update(update_data)
 
 end
 
