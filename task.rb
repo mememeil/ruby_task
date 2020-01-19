59211d0b4600497d2ab3d21be521f6ed08d457f4
mememeil <kodhikodhi@gmail.com>
2020-01-19 11:49:34 +0900

    Write answer for Q14 to task.rb

--- task.rb
+++ task.rb
@@ -165,9 +165,12 @@ def q13
 end
 
 def q14
+  # Q14. 次の配列から全てのキーを取り出した配列を作成し，出力して下さい。
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
+  array = data.keys
+  p array
 
 end
 
